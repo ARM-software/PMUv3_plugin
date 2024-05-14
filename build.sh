14fc19a3b2aca05169bddfe03ebaf0c9d0e578b5
gayyeg01 <gayathrinarayana.yegnanarayanan@arm.com>
2024-05-13 23:46:15 -0500

    final changes

--- build.sh
+++ build.sh
@@ -17,6 +17,5 @@ gcc -c processing.c -I/home/ubuntu/ut_integration/linux/tools/lib/perf/include -
 
 ar rcs libpmuv3_plugin.a pmuv3_plugin.o
 ar rcs libpmuv3_plugin_bundle.a pmuv3_plugin_bundle.o processing.o
-#ar rcs libpmuv3_plugin_bundle.a pmuv3_plugin_bundle.o processing.o processing_wrapper.o
 #ar rcs libpmuv3_plugin_bundle.a pmuv3_plugin_bundle.o  processing_c.o
 
